f49f7c85f2f54494a2ca91413bb88f8888ca06a5
te-tsoprek <tsoprek@thousandeyes.com>
2023-04-17 11:22:26 +0200

    Added configured proxy check def  previous_proxy function checks if proxy was configured previously

--- main.py
+++ main.py
@@ -7,24 +7,26 @@ import ssl
 import urllib.error
 import time
 
-
-lb=bool()
-proxy=''
+lb = bool()
+proxy = ''
 proxy_type = ''
-os_upgrade_urls = ['http://apt.thousandeyes.com', 'http://changelogs.ubuntu.com', 'http://archive.canonical.com', 'https://changelogs.ubuntu.com', 'https://apt.thousandeyes.com']
-reg_url='https://registry.agt.thousandeyes.com'
-sc1_url='https://sc1.thousandeyes.com'
-c1_url='https://c1.thousandeyes.com'
-data_url='https://data1.agt.thousandeyes.com'
-all_urls=[reg_url, sc1_url, c1_url, data_url]
+os_upgrade_urls = ['http://apt.thousandeyes.com', 'http://changelogs.ubuntu.com', 'http://archive.canonical.com',
+                   'https://changelogs.ubuntu.com', 'https://apt.thousandeyes.com']
+reg_url = 'https://registry.agt.thousandeyes.com'
+sc1_url = 'https://sc1.thousandeyes.com'
+c1_url = 'https://c1.thousandeyes.com'
+data_url = 'https://data1.agt.thousandeyes.com'
+all_urls = [reg_url, sc1_url, c1_url, data_url]
 yum_path = '/etc/yum.repos.d/thousandeyes.repo'
 apt_path = '/etc/apt/sources.list.d/thousandeyes.list'
 os_info = []
-yum_config_rh8=['[thousandeyes]', 'name=ThousandEyes', 'baseurl=https://yum.thousandeyes.com/RHEL/8/x86_64/', 'gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-thousandeyes', 'gpgcheck=1']
-yum_config_rh7=['[thousandeyes]', 'name=ThousandEyes', 'baseurl=https://yum.thousandeyes.com/RHEL/7/x86_64/', 'gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-thousandeyes', 'gpgcheck=1']
-apt_config_file="deb https://apt.thousandeyes.com focal main"
+yum_config_rh8 = ['[thousandeyes]', 'name=ThousandEyes', 'baseurl=https://yum.thousandeyes.com/RHEL/8/x86_64/',
+                  'gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-thousandeyes', 'gpgcheck=1']
+yum_config_rh7 = ['[thousandeyes]', 'name=ThousandEyes', 'baseurl=https://yum.thousandeyes.com/RHEL/7/x86_64/',
+                  'gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-thousandeyes', 'gpgcheck=1']
+apt_config_file = "deb https://apt.thousandeyes.com focal main"
 os_release_path = '/etc/os-release'
-yum_gpg_key=""""-----BEGIN PGP PUBLIC KEY BLOCK-----
+yum_gpg_key = """"-----BEGIN PGP PUBLIC KEY BLOCK-----
 Version: GnuPG v1.4.11 (GNU/Linux)
 
 mQENBFApO8oBCACxHESumhIcqUTvpIA+q9yIWQQL2nE1twF1T92xIJ9kgOF/5ali
@@ -44,7 +46,8 @@ Qty56ztU
 =p3tN
 -----END PGP PUBLIC KEY BLOCK-----"""
 
-#Opens file for reading and check for regex NAME and VERSION which are added to os_version list
+
+# Opens file for reading and check for regex NAME and VERSION which are added to os_version list
 def read_os_version(file, *args):
     with open(file) as f:
         for line in f:
@@ -52,15 +55,17 @@ def read_os_version(file, *args):
                 line = line[6:-2]
                 os_info.append(line.strip())
             elif re.search('VERSION_ID', line) and line.startswith('VERSION_ID'):
-                line=line[12:-2]
+                line = line[12:-2]
                 os_info.append(line.strip())
         f.close()
+
 
 def check_repo(os_name):
     # Use a breakpoint in the code line below to debug your script.
     if os_name == 'Red Hat Enterprise Linux':
-        yum_conf=os.path.isfile(yum_path)
-        os_version=os_info[1]
+        yum_conf = os.path.isfile(yum_path)
+        os_version = os_info[1]
+
         if not yum_conf:
             print("***File {} doesn't exist. Creating repo file...***".format(yum_path))
             if os_version.startswith('7'):
@@ -69,11 +74,11 @@ def check_repo(os_name):
                     for yum_item in yum_config_rh7:
                         yum_conf_write.write(yum_item)
                         yum_conf_write.write("\n")
-                    if proxy_use=='y':
+                    if proxy_use == 'y':
                         yum_conf_write.write('proxy="{}://{}"'.format(proxy_type, proxy))
                         yum_conf_write.write("\n")
                         yum_auth = input('***Would you like to configure authentication for proxy?(y/n)***\n')
-                        if yum_auth =='y':
+                        if yum_auth == 'y':
                             proxy_user = input('***Add proxy username.***\n')
                             yum_conf_write.write('proxy_username="{}"'.format(proxy_user))
                             yum_conf_write.write("\n")
@@ -103,7 +108,7 @@ def check_repo(os_name):
                         print(line)
 
     elif os_name == 'Ubuntu':
-        apt_conf=os.path.isfile(apt_path)
+        apt_conf = os.path.isfile(apt_path)
         if not apt_conf:
             print("File {} doesn't exist. Creating repo file.".format(apt_path))
             with open(apt_path, "w") as apt_conf_write:
@@ -119,9 +124,9 @@ def check_gpg_key():
     if os_info[0] == 'Red Hat Enterprise Linux':
         yum_gpg_path = '/etc/pki/rpm-gpg/RPM-GPG-KEY-thousandeyes'
         yum_gpg_file = os.path.isfile(yum_gpg_path)
-        rpm_list_cmd=("rpm", "-q", "gpg-pubkey", "--qf", "'%{NAME}-%{VERSION}-%{RELEASE}\t%{SUMMARY}\n'")
+        rpm_list_cmd = ("rpm", "-q", "gpg-pubkey", "--qf", "'%{NAME}-%{VERSION}-%{RELEASE}\t%{SUMMARY}\n'")
         rpm_key_list = subprocess.run(rpm_list_cmd, stdout=subprocess.PIPE)
-        yum_key_in_rpm = re.search('ThousandEyes',str(rpm_key_list))
+        yum_key_in_rpm = re.search('ThousandEyes', str(rpm_key_list))
         if not yum_gpg_file or not yum_key_in_rpm:
             print("***File {} doesn't exist.")
             create_yum_repo = input("***Would you like to download and import gpg key to rpm?(y/n)***\n").lower()
@@ -130,7 +135,7 @@ def check_gpg_key():
                 with open(yum_gpg_path, 'w') as yum_gpg_write:
                     yum_gpg_write.write(yum_gpg_key)
                     yum_import_cmd = ("sudo", "rpm", "--import", "/etc/pki/rpm-gpg/RPM-GPG-KEY-thousandeyes")
-                    subprocess.run(yum_import_cmd, stdout=subprocess.PIPE )
+                    subprocess.run(yum_import_cmd, stdout=subprocess.PIPE)
                     print("***GPG IMPORT LOGS***")
                     print("***RPM import requires sudo permissions.***")
                     print("***While GPG key is set to 1 and GPG path is added to repo config, this should work.***")
@@ -141,15 +146,16 @@ def check_gpg_key():
                 print('***GPG key file was NOT found or added to RPM.***')
 
     elif os_info[0] == 'Ubuntu':
-        list_apt_keys=subprocess.run(["apt-key", "list"], stdout=subprocess.PIPE)
+        list_apt_keys = subprocess.run(["apt-key", "list"], stdout=subprocess.PIPE)
         if re.search('ThousandEyes', str(list_apt_keys.stdout)):
             print('***Found key in apt-key list. Skipping adding file. Do additional check manually.***')
 
         else:
             print("***Key is not in apt-key list. Creating key file and importing...***")
-            create_apt_repo=input("***Would you like to download and import gpg key to apt?(y/n)***\n").lower()
-            if create_apt_repo=='y':
-                subprocess.run(["curl", "https://apt.thousandeyes.com/thousandeyes-apt-key.pub", "--output", "apt_key.pub"])
+            create_apt_repo = input("***Would you like to download and import gpg key to apt?(y/n)***\n").lower()
+            if create_apt_repo == 'y':
+                subprocess.run(
+                    ["curl", "https://apt.thousandeyes.com/thousandeyes-apt-key.pub", "--output", "apt_key.pub"])
                 time.sleep(3)
                 subprocess.run(["apt-key", "add", "apt_key.pub"])
             else:
@@ -164,30 +170,62 @@ def test_connectivity(url):
     ctx.verify_mode = ssl.CERT_NONE
     if proxy:
         try:
-            req=urllib.request.Request(url)
-            req.set_proxy(proxy,'http')
+            req = urllib.request.Request(url)
+            req.set_proxy(proxy, 'http')
             url_response = urllib.request.urlopen(req, context=ctx)
             return url_response.getcode()
         except urllib.error.HTTPError as err:
             return err.code
     else:
         try:
-            url_response=urllib.request.urlopen(url, context=ctx)
+            url_response = urllib.request.urlopen(url, context=ctx)
             return url_response.getcode()
         except urllib.error.HTTPError as err:
             return err.code
+
+
+def previous_proxy(os_name):
+    if os_name == 'Red Hat Enterprise Linux':
+        yum_dir = '/etc/yum/repos.d/'
+        yum_dir_ls = os.listdir(yum_dir)
+        for filename in yum_dir_ls:
+            with open(yum_dir+filename, 'r') as current_file:
+                for line in current_file:
+                    proxy_line_search = re.search('proxy=', line)
+                    if proxy_line_search:
+                        print('***Found proxy in {}.***\n'.format(current_file))
+                        print('*** Proxy in file: {}.\n'.format(line))
+            if not proxy_line_search:
+                print('***No proxy configuration found in {}.***\n'.format(yum_dir+filename))
+    elif os_name == 'Ubuntu':
+        apt_dir = '/etc/apt/apt.conf.d/'
+        apt_dir_ls = os.listdir(apt_dir)
+        for filename in apt_dir_ls:
+            with open(apt_dir+filename, 'r') as current_file:
+                for line in current_file:
+                    proxy_line_search = re.search(r'acquire::http*::proxy', line.lower())
+                    if proxy_line_search:
+                        print('***Found proxy in {}.***\n'.format(apt_dir+filename))
+                        print('*** Proxy configuration in file: {}.\n'.format(line))
+            if not proxy_line_search:
+                print('***No proxy configuration found in {}.***\n'.format(apt_dir+filename))
+    else:
+        print('***Skipping check, unsupported OS.***')
+
+
 if __name__ == '__main__':
+    read_os_version(os_release_path)
+    previous_proxy(os_info[0])
     while not lb:
         proxy_use = input('***Would you like to use proxy?***\n').lower()
         if proxy_use == 'y':
             proxy = input('***Add proxy hostname:port if you use proxy, example (my.proxy.url:80).***\n').lower()
             proxy_type = input('***Would you like to add HTTP or HTTPS proxy?(http/https)***\n').lower()
-            lb=True
-        elif proxy_use=='n':
-            lb=True
+            lb = True
+        elif proxy_use == 'n':
+            lb = True
         else:
             continue
-    read_os_version(os_release_path)
     check_repo(os_info[0])
     check_gpg_key()
     for item in all_urls:
@@ -208,6 +246,5 @@ if __name__ == '__main__':
             else:
                 print('Connectivity to {} has FAILED!'.format(upgrade_item))
                 print('***HTTP Response code:', apt_response, '***')
-
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
